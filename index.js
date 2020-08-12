0a6373dbef6ce3b21d86d260b5bb06fa442da2e9
sarah rose <xsarahrosemeow@gmail.com>
2020-08-12 03:00:19 -0400

    work in progress

---
+++ index.js
@@ -0,0 +1,638 @@
+const cats = [
+  {
+    Name: "Promise",
+    Sex: "Male",
+    Age: 2,
+    Breed: "Tuxedo",
+    "Favorite Toy": "Ball",
+    "Biggest Fear": "Snakes",
+    Color: "Black and White",
+    Eyes: "Grey",
+    "Birthday Year": 2018,
+    "Adoption Year": 2020,
+    Status: "House",
+  },
+  {
+    Name: "Persia",
+    Sex: "Female",
+    Age: 2,
+    Breed: "Siamese",
+    "Favorite Toy": "Mouse",
+    "Biggest Fear": "Paper",
+    Color: "Brown",
+    Eyes: "Blue",
+    "Birthday Year": 2018,
+    "Adoption Year": 2020,
+    Status: "Stray",
+  },
+  {
+    Name: "Monster",
+    Sex: "Male",
+    Age: 2,
+    Breed: "Himalayan",
+    "Favorite Toy": "Laser",
+    "Biggest Fear": "Dogs",
+    Color: "Brown",
+    Eyes: "Green",
+    "Birthday Year": 2018,
+    "Adoption Year": 2018,
+    Status: "Stray",
+  },
+  {
+    Name: "Big Girl",
+    Sex: "Female",
+    Age: 10,
+    Breed: "Tabby",
+    "Favorite Toy": "Feather",
+    "Biggest Fear": "Cats",
+    Color: "Brown",
+    Eyes: "Brown",
+    "Birthday Year": 2010,
+    "Adoption Year": 2017,
+    Status: "Stray",
+  },
+  {
+    Name: "Momma",
+    Sex: "Female",
+    Age: 12,
+    Breed: "Calico",
+    "Favorite Toy": "Birds",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Yellow",
+    "Birthday Year": 2008,
+    "Adoption Year": 2015,
+    Status: "Stray",
+  },
+  {
+    Name: "Arabella",
+    Sex: "Female",
+    Age: 13,
+    Breed: "Persian",
+    "Favorite Toy": "String",
+    "Biggest Fear": "Cats",
+    Color: "Grey",
+    Eyes: "Green",
+    "Birthday Year": 2007,
+    "Adoption Year": 2010,
+    Status: "Stray",
+  },
+  {
+    Name: "Velvet",
+    Sex: "Female",
+    Age: 2,
+    Breed: "Absidian",
+    "Favorite Toy": "Electric Butterfly",
+    "Biggest Fear": "Mice",
+    Color: "Brown",
+    Eyes: "Orange",
+    "Birthday Year": 2018,
+    "Adoption Year": 2019,
+    Status: "House",
+  },
+  {
+    Name: "Tommy",
+    Sex: "Male",
+    Age: 4,
+    Breed: "Manx",
+    "Favorite Toy": "Bird",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Brown",
+    "Birthday Year": 2017,
+    "Adoption Year": 2017,
+    Status: "Stray",
+  },
+  {
+    Name: "Scruffy",
+    Sex: "Female",
+    Age: 5,
+    Breed: "Calico",
+    "Favorite Toy": "Fish",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Blue",
+    "Birthday Year": 2015,
+    "Adoption Year": 2015,
+    Status: "Stray",
+  },
+  {
+    Name: "Jackson",
+    Sex: "Male",
+    Age: 2,
+    Breed: "Tuxedo",
+    "Favorite Toy": "Ball",
+    "Biggest Fear": "Snakes",
+    Color: "Black and White",
+    Eyes: "Grey",
+    "Birthday Year": 2018,
+    "Adoption Year": 2020,
+    Status: "House",
+  },
+  {
+    Name: "Angel",
+    Sex: "Female",
+    Age: 2,
+    Breed: "Siamese",
+    "Favorite Toy": "Mouse",
+    "Biggest Fear": "Paper",
+    Color: "Brown",
+    Eyes: "Blue",
+    "Birthday Year": 2018,
+    "Adoption Year": 2020,
+    Status: "Stray",
+  },
+  {
+    Name: "Toby",
+    Sex: "Male",
+    Age: 2,
+    Breed: "Himalayan",
+    "Favorite Toy": "Laser",
+    "Biggest Fear": "Dogs",
+    Color: "Brown",
+    Eyes: "Green",
+    "Birthday Year": 2018,
+    "Adoption Year": 2018,
+    Status: "Stray",
+  },
+  {
+    Name: "Keira",
+    Sex: "Female",
+    Age: 10,
+    Breed: "Tabby",
+    "Favorite Toy": "Feather",
+    "Biggest Fear": "Cats",
+    Color: "Brown",
+    Eyes: "Brown",
+    "Birthday Year": 2010,
+    "Adoption Year": 2017,
+    Status: "Stray",
+  },
+  {
+    Name: "Felina",
+    Sex: "Female",
+    Age: 12,
+    Breed: "Calico",
+    "Favorite Toy": "Birds",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Yellow",
+    "Birthday Year": 2008,
+    "Adoption Year": 2015,
+    Status: "Stray",
+  },
+  {
+    Name: "Tommy Girl",
+    Sex: "Female",
+    Age: 13,
+    Breed: "Persian",
+    "Favorite Toy": "String",
+    "Biggest Fear": "Cats",
+    Color: "Grey",
+    Eyes: "Green",
+    "Birthday Year": 2007,
+    "Adoption Year": 2010,
+    Status: "Stray",
+  },
+  {
+    Name: "Serena",
+    Sex: "Female",
+    Age: 2,
+    Breed: "Absidian",
+    "Favorite Toy": "Electric Butterfly",
+    "Biggest Fear": "Mice",
+    Color: "Brown",
+    Eyes: "Orange",
+    "Birthday Year": 2018,
+    "Adoption Year": 2019,
+    Status: "House",
+  },
+  {
+    Name: "Zeus",
+    Sex: "Male",
+    Age: 4,
+    Breed: "Manx",
+    "Favorite Toy": "Bird",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Brown",
+    "Birthday Year": 2017,
+    "Adoption Year": 2017,
+    Status: "Stray",
+  },
+  {
+    Name: "Winona",
+    Sex: "Female",
+    Age: 5,
+    Breed: "Calico",
+    "Favorite Toy": "Fish",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Blue",
+    "Birthday Year": 2015,
+    "Adoption Year": 2015,
+    Status: "Stray",
+  },
+  {
+    Name: "Oscar",
+    Sex: "Male",
+    Age: 2,
+    Breed: "Tuxedo",
+    "Favorite Toy": "Ball",
+    "Biggest Fear": "Snakes",
+    Color: "Black and White",
+    Eyes: "Grey",
+    "Birthday Year": 2018,
+    "Adoption Year": 2020,
+    Status: "House",
+  },
+  {
+    Name: "Cleo",
+    Sex: "Female",
+    Age: 2,
+    Breed: "Siamese",
+    "Favorite Toy": "Mouse",
+    "Biggest Fear": "Paper",
+    Color: "Brown",
+    Eyes: "Blue",
+    "Birthday Year": 2018,
+    "Adoption Year": 2020,
+    Status: "Stray",
+  },
+  {
+    Name: "Simba",
+    Sex: "Male",
+    Age: 2,
+    Breed: "Himalayan",
+    "Favorite Toy": "Laser",
+    "Biggest Fear": "Dogs",
+    Color: "Brown",
+    Eyes: "Green",
+    "Birthday Year": 2018,
+    "Adoption Year": 2018,
+    Status: "Stray",
+  },
+  {
+    Name: "Lucy",
+    Sex: "Female",
+    Age: 10,
+    Breed: "Tabby",
+    "Favorite Toy": "Feather",
+    "Biggest Fear": "Cats",
+    Color: "Brown",
+    Eyes: "Brown",
+    "Birthday Year": 2010,
+    "Adoption Year": 2017,
+    Status: "Stray",
+  },
+  {
+    Name: "Daisy",
+    Sex: "Female",
+    Age: 12,
+    Breed: "Calico",
+    "Favorite Toy": "Birds",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Yellow",
+    "Birthday Year": 2008,
+    "Adoption Year": 2015,
+    Status: "Stray",
+  },
+  {
+    Name: "Ginger",
+    Sex: "Female",
+    Age: 13,
+    Breed: "Persian",
+    "Favorite Toy": "String",
+    "Biggest Fear": "Cats",
+    Color: "Grey",
+    Eyes: "Green",
+    "Birthday Year": 2007,
+    "Adoption Year": 2010,
+    Status: "Stray",
+  },
+  {
+    Name: "Minnie",
+    Sex: "Female",
+    Age: 2,
+    Breed: "Absidian",
+    "Favorite Toy": "Electric Butterfly",
+    "Biggest Fear": "Mice",
+    Color: "Brown",
+    Eyes: "Orange",
+    "Birthday Year": 2018,
+    "Adoption Year": 2019,
+    Status: "House",
+  },
+  {
+    Name: "George",
+    Sex: "Male",
+    Age: 4,
+    Breed: "Manx",
+    "Favorite Toy": "Bird",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Brown",
+    "Birthday Year": 2017,
+    "Adoption Year": 2017,
+    Status: "Stray",
+  },
+  {
+    Name: "Holly",
+    Sex: "Female",
+    Age: 5,
+    Breed: "Calico",
+    "Favorite Toy": "Fish",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Blue",
+    "Birthday Year": 2015,
+    "Adoption Year": 2015,
+    Status: "Stray",
+  },
+  {
+    Name: "Smokey",
+    Sex: "Male",
+    Age: 2,
+    Breed: "Tuxedo",
+    "Favorite Toy": "Ball",
+    "Biggest Fear": "Snakes",
+    Color: "Black and White",
+    Eyes: "Grey",
+    "Birthday Year": 2018,
+    "Adoption Year": 2020,
+    Status: "House",
+  },
+  {
+    Name: "Kitty",
+    Sex: "Female",
+    Age: 2,
+    Breed: "Siamese",
+    "Favorite Toy": "Mouse",
+    "Biggest Fear": "Paper",
+    Color: "Brown",
+    Eyes: "Blue",
+    "Birthday Year": 2018,
+    "Adoption Year": 2020,
+    Status: "Stray",
+  },
+  {
+    Name: "Oreo",
+    Sex: "Male",
+    Age: 2,
+    Breed: "Himalayan",
+    "Favorite Toy": "Laser",
+    "Biggest Fear": "Dogs",
+    Color: "Brown",
+    Eyes: "Green",
+    "Birthday Year": 2018,
+    "Adoption Year": 2018,
+    Status: "Stray",
+  },
+  {
+    Name: "Midnight",
+    Sex: "Female",
+    Age: 10,
+    Breed: "Tabby",
+    "Favorite Toy": "Feather",
+    "Biggest Fear": "Cats",
+    Color: "Brown",
+    Eyes: "Brown",
+    "Birthday Year": 2010,
+    "Adoption Year": 2017,
+    Status: "Stray",
+  },
+  {
+    Name: "Baby",
+    Sex: "Female",
+    Age: 12,
+    Breed: "Calico",
+    "Favorite Toy": "Birds",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Yellow",
+    "Birthday Year": 2008,
+    "Adoption Year": 2015,
+    Status: "Stray",
+  },
+  {
+    Name: "Precious",
+    Sex: "Female",
+    Age: 13,
+    Breed: "Persian",
+    "Favorite Toy": "String",
+    "Biggest Fear": "Cats",
+    Color: "Grey",
+    Eyes: "Green",
+    "Birthday Year": 2007,
+    "Adoption Year": 2010,
+    Status: "Stray",
+  },
+  {
+    Name: "Pepper",
+    Sex: "Female",
+    Age: 2,
+    Breed: "Absidian",
+    "Favorite Toy": "Electric Butterfly",
+    "Biggest Fear": "Mice",
+    Color: "Brown",
+    Eyes: "Orange",
+    "Birthday Year": 2018,
+    "Adoption Year": 2019,
+    Status: "House",
+  },
+  {
+    Name: "Shadow",
+    Sex: "Male",
+    Age: 4,
+    Breed: "Manx",
+    "Favorite Toy": "Bird",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Brown",
+    "Birthday Year": 2017,
+    "Adoption Year": 2017,
+    Status: "Stray",
+  },
+  {
+    Name: "Callie",
+    Sex: "Female",
+    Age: 5,
+    Breed: "Calico",
+    "Favorite Toy": "Fish",
+    "Biggest Fear": "Vacuum",
+    Color: "Orange",
+    Eyes: "Blue",
+    "Birthday Year": 2015,
+    "Adoption Year": 2015,
+    Status: "Stray",
+  },
+];
+
+// console.log("entire array", cats);
+
+// console.log(cats);
+
+/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data
+(a) Name of Stray Cats born in 2018
+(b) Sex of Stray cats cats born in 2018
+(c) Adoption year of stray cats born in 2018
+(d) Age of  stray cats born in 2018
+(e) Oldest stray cat born in 2018 */
+
+//1A
+let straycats2018 = cats.filter((item) => {
+  return item.Status === "Stray" && item["Birthday Year"] === 2018;
+});
+
+console.log("The answer to task 1A is: ", straycats2018);
+
+function getNames(item, data) {
+  return item.Name;
+}
+
+// console.log(getNames(straycats2018));
+let mapCats = straycats2018.map(getNames).join("\n");
+
+console.log("The answer to Task 1A shortened is: ", mapCats);
+
+// 1b//
+function getSex(item) {
+  return item.Sex;
+}
+
+let sexOfCats = straycats2018.map(getSex).join(`\n`);
+console.log("The answer to Task 1B is: ", sexOfCats);
+
+let result = straycats2018.map(
+  (item) =>
+    item.Name +
+    ": " +
+    item.Sex +
+    " was born in " +
+    item["Birthday Year"] +
+    " and " +
+    "adopted in " +
+    item["Adoption Year"] +
+    " at age " +
+    item.Age
+);
+console.log("The answer to Task A-E combined is: ", result);
+
+//TASK 2//
+/* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
+
+// CATS TASK TWO
+// /* Task 2: Create a function called  getStrays that takes `data` as an argument and returns an array of objects with only Stray data */
+
+let getStrays = cats.filter((item) => {
+  return item.Status === "Stray";
+});
+
+console.log("The Answer to Task 2 is ", getStrays);
+
+// TASK 3 //
+//  Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset
+
+// /* Task 3: Implement a higher-order function called `getAge` that accepts the callback function `getStrays`, and returns an array called `age` containing all of the Ages in the dataset */
+
+let getAge = getStrays.map((item) => {
+  let age = [];
+  age = item.Age;
+  return age;
+});
+
+console.log("The answer to Task 3 is ", getAge);
+
+let ageAndName = getStrays.map((item) => {
+  return item.Name + ": age: " + item.Age;
+});
+
+console.log("Nicer version of Task 3 ", ageAndName);
+
+/* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */
+
+/* Task 4: Implement a higher-order function called `getHowLongWereYouWaiting`, that accepts the callback function `getStrays()` and determines how long Stray cats were waiting to be adopted
+Return the name of all strays that were adopted within one year called luckyCats` */
+
+// Task 5: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!"
+// Parameters:
+// callback function getWinners
+// callback function getYears
+
+// Task 5: Implement a higher-order function called `getAdoptionYear` that accepts the following parameters and returns a set of strings "{name} was adopted in {year}; less than a year!"
+
+// /* Task 6: Write a function called `getAverageWaitingToBeAdoptedYear` that accepts a parameter `data` and returns the the average number of years the strays and hoyse cats were waiting to be adopted  (Hint: use .reduce and do this in 2 steps) */
+
+// PRACTICE BY AJA'S CHEATSHEET
+
+// const heroes = [
+//   { name: "Hulk", strength: 90000 },
+//   { name: "Spider-Man", strength: 25000 },
+//   { name: "Hawk Eye", strength: 136 },
+//   { name: "Thor", strength: 100000 },
+//   { name: "Black Widow", strength: 136 },
+//   { name: "Vision", strength: 5000 },
+//   { name: "Scarlet Witch", strength: 60 },
+//   { name: "Mystique", strength: 120 },
+//   { name: "Namora", strength: 75000 },
+//   { name: "Captain America", strength: 362 },
+//   { name: "Deadpool", strength: 1814 },
+//   { name: "Black Panther", strength: 1814 },
+// ];
+// // ----------------------------------------------------------
+// //
+
+// // ------------------MAP. ---------------------
+// // function getName(item) {
+// //   return item.name;
+// // }
+// // const names = heroes.map(getName);
+
+// // console.log(names); /// returns all names, "Hulk, Spider-man, Hawk Eye, etc"
+
+// // console.log(getName);
+
+// // function getBreeds(item) {
+// //   return item.Breed
+// // }
+
+// // const breeds = cats.map(getBreeds);
+// // console.log(breeds); // returns all breeds Tuxedo, Persian, etc
+
+// // ---------- FILTER-------------------------------
+
+// // The .filter() method takes your array and removes items that don't pass a test you give it.
+// // EXAMPLE:
+
+// // function strong(hero) {
+// //   return hero.name === "Hulk" && hero.strength >= 200;
+// // }
+// // const tuff = heroes.filter(strong);
+// // console.log(tuff); // returns name: Hulk: strength: 90000, and only all others it applies to
+
+// // function getWaitTimeUnderTwo(item) {
+// //   if (item["Adoption Year"] === item["Birthday Year"]) {
+// //     return item.Name;
+// //   }
+// // }
+
+// // getWaitTimeUnderTwo(cats);
+
+// // const luckyCats = cats.filter(getWaitTimeUnderTwo);
+// // console.log(luckyCats);
+
+// // function getName(hero) {
+// //   return hero.name;
+// // }
+// // const list = heroes.map(getName).join("cat");
+
+// // console.log(list);
+
+// function sumStrength(total, hero) {
+//   return total + hero.strength;
+// }
+
+// const totalStength = heroes.reduce(sumStrength, 0);
+
+// console.log(totalStength);
